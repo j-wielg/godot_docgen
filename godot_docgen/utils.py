b59797a3cb7fdec565cd9e4a00d64b7f90df4830
j-wielg <wielgus974@gmail.com>
2024-03-21 18:31:54 -0400

    feat: Added print_warning to `utils.py` Added a function to print a formatted warning to the utils file. Also wrote docstrings for both print_error and print_warning.

--- godot_docgen/utils.py
+++ godot_docgen/utils.py
@@ -1,6 +1,8 @@
 '''
 Contains utility functions
 '''
+import sys
+
 
 STYLES = {
     "red": "\x1b[91m",  # ]
@@ -13,8 +15,51 @@ STYLES = {
 
 
 def print_error(error: str, state) -> None:
+    '''
+    Takes an error message, prints a formatted error, and
+    increments the error count in the state.
+
+    Parameters
+    ----------
+    error : str
+        The error message to print
+    state : State
+        The global state of the program.
+    '''
     if state.should_color:
-        print(f'{STYLES["red"]}{STYLES["bold"]}ERROR:{STYLES["regular"]} {error}{STYLES["reset"]}')
+        print(f'{STYLES["red"]}{STYLES["bold"]}ERROR:{STYLES["regular"]} {error}{STYLES["reset"]}', file=sys.stderr)
     else:
         print(f'ERROR: {error}')
     state.num_errors += 1
+
+
+def print_warning(warning: str, state) -> None:
+    '''
+    Takes a warning message, prints a formatted warning, and
+    increments the warning count in the state.
+
+    Parameters
+    ----------
+    warning : str
+        The warning message to print
+    state : State
+        The global state of the program.
+    '''
+    if state.should_color:
+        print(f'{STYLES["yellow"]}{STYLES["bold"]}WARNING:{STYLES["regular"]} {warning}{STYLES["reset"]}', file=sys.stderr)
+    else:
+        print(f'WARNING: {warning}')
+    state.num_warnings += 1
+
+
+def make_heading(title: str, underline: str, l10n: bool = True) -> str:
+    if l10n:
+        new_title = translate(title)
+        if new_title != title:
+            title = new_title
+            underline *= 2  # Double length to handle wide chars.
+    return f"{title}\n{(underline * len(title))}\n\n"
+
+
+def translate(line: str):
+    return line
