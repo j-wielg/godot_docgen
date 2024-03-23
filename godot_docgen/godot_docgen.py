f14489417920e3e95d75c1bde6f675eab9315cb2
j-wielg <wielgus974@gmail.com>
2024-03-23 19:24:02 -0400

    feat: Added log messages to `load_files` Used the utils.print_log function in load_files to display information to the console during the parsing process.

--- godot_docgen/godot_docgen.py
+++ godot_docgen/godot_docgen.py
@@ -3,6 +3,7 @@ import definitions
 from scenedef import SceneDef, NodeDef, ScenesNotParsed
 from scriptdef import ScriptDef, ScriptGroups
 from pathlib import Path
+import utils
 
 
 def generate_documentation():
@@ -11,6 +12,9 @@ def generate_documentation():
     '''
     # Holds the settings data and other global stuff
     state = State()
+    # Loads files into the state object
+    load_files(state)
+    # Generates rst for classes
 
 
 def load_files(state: State):
@@ -29,6 +33,10 @@ def load_files(state: State):
     xml: set[Path] = set()
     tscn: list[Path] = []
     # Finds all relevant files
+    utils.print_log(
+        f'Searching {str(path)} for files...',
+        state
+    )
     while unvisited:
         file = unvisited.pop()
         filename = file.name
@@ -43,18 +51,40 @@ def load_files(state: State):
             for subfile in file.iterdir():
                 unvisited.append(subfile)
     # Generates all script files and adds them to the state
+    utils.print_log(
+        f'Parsing {len(xml)} xml files...',
+        state
+    )
     for script_path in xml:
         script = ScriptDef(state)
         script.parse_file(str(script_path))
     # Generates scene objects and adds them to the state
+    utils.print_log(
+        f'Parsing {len(tscn)} scene files...',
+        state
+    )
     while tscn:
-        scene_path = tscn.pop()
-        scene = SceneDef(state)
-        try:
-            scene.parse_file(scene_path)
-        except ScenesNotParsed:
-            tscn.insert(0, scene_path)
-            continue
+        skipped = 0
+        size = len(tscn)
+        for i in range(size - 1, -1, -1):
+            scene_path = tscn[i]
+            scene = SceneDef(state)
+            try:
+                scene.parse_file(scene_path)
+            except ScenesNotParsed:
+                skipped += 1
+                continue
+            scene_path = tscn.pop(i)
+        if skipped == size:
+            utils.print_error(f'Failed to parse {size} scenes:', state)
+            for scene_path in tscn:
+                utils.print_error(f'Could not parse {scene_path}', state)
+            break
+    # Attaches scenes to scripts
+    utils.print_log(
+        'Attaching scripts to scenes...',
+        state
+    )
     for scene_path in state.scenes:
         scene: SceneDef = state.scenes[scene_path]
         scene.connect_scripts()
